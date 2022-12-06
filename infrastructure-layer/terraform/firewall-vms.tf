40958bf1a4fb0b329370c1d7da45f1f9fd3fa31a
Brian Brookman <bcbrookman@users.noreply.github.com>
2022-12-06 15:37:03 -0800

    Move Terraform files to infrastructure-layer
    Since Terrraform would be first used to provision network-related
    VMs in the infrastructure layer, Terraform configuration files
    should appear there first, and can be symlinked in upper layers.

---
+++ infrastructure-layer/terraform/firewall-vms.tf
@@ -0,0 +1,26 @@
+resource "proxmox_vm_qemu" "pfsense" {
+  count = 2
+  name = "pfsense0${count.index % 2 + 1}"
+  target_node = "pve0${count.index % 2 + 1}"
+  full_clone = false
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+  scsihw = "virtio-scsi-pci"
+  onboot = true
+  boot = "order=ide0;net0"
+  qemu_os = "other"
+  disk {
+    size = "10G"
+    type = "scsi"
+    storage = "local"
+    iothread = 1
+  }
+  lifecycle {
+    # prevent_destroy = true
+    ignore_changes = [
+      network,
+    ]
+  }
+}
---
+++ infrastructure-layer/terraform/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  cloud {
+    organization = "bcbrookman"
+    workspaces {
+      name = "homelab"
+    }
+  }
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+    }
+    sops = {
+      source = "carlpett/sops"
+    }
+  }
+}
+
+data "sops_file" "tfvars" {
+  source_file = "tfvars.yaml"
+}
+
+variable "pm_api_url" {
+  sensitive = true
+  default = "https://pve:8006/api2/json"
+}
+
+variable "pm_api_token_id" {
+  sensitive = true
+  default = "user@pam!token"
+}
+
+variable "pm_api_token_secret" {
+  sensitive = true
+  default = "token"
+}
+
+variable "sshkeys" {
+  sensitive = true
+  default = "ssh-public-key"
+}
+
+provider "proxmox" {
+  pm_api_url = data.sops_file.tfvars.data["pm_api_url"]
+  pm_api_token_id = data.sops_file.tfvars.data["pm_api_token_id"]
+  pm_api_token_secret = data.sops_file.tfvars.data["pm_api_token_secret"]
+  pm_tls_insecure = true
+}
