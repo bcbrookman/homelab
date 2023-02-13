2c592cc64d279f04e01b6346a947fccb1dbd073a
Brian Brookman <bcbrookman@users.noreply.github.com>
2023-02-13 08:29:24 -0800

    Decommission docker01

--- platform-layer/terraform/other-vms.tf
+++ platform-layer/terraform/other-vms.tf
@@ -28,34 +28,3 @@ resource "proxmox_vm_qemu" "plex01" {
     ]
   }
 }
-
-resource "proxmox_vm_qemu" "docker01" {
-  name        = "docker01"
-  target_node = "pve02"
-  full_clone  = false
-  agent       = 1
-  cores       = 2
-  sockets     = 1
-  cpu         = "host"
-  memory      = 4096
-  scsihw      = "virtio-scsi-pci"
-  onboot      = true
-  boot        = "order=scsi0;net0"
-  disk {
-    size    = "32G"
-    type    = "scsi"
-    storage = "local-lvm"
-    discard = "on"
-  }
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-    tag    = 20
-  }
-  lifecycle {
-    # prevent_destroy = true
-    ignore_changes = [
-      network,
-    ]
-  }
-}
