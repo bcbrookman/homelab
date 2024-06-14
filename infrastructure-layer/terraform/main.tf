a709980a88b2af3117669d677fdc53f5c000d4a8
Brian Brookman <bcbrookman@users.noreply.github.com>
2024-06-13 17:58:22 -0700

    Upgrade to Terraform provider telmate/proxmox v3
    Support for Proxmox VE 8.x will be introduced in v3.0.1.
    Using the RC version now will support upgrading to PVE 8.x.
    before it goes EoL starting 8/2024.

--- infrastructure-layer/terraform/main.tf
+++ infrastructure-layer/terraform/main.tf
@@ -7,7 +7,8 @@ terraform {
   }
   required_providers {
     proxmox = {
-      source = "telmate/proxmox"
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc3"
     }
     sops = {
       source = "carlpett/sops"
