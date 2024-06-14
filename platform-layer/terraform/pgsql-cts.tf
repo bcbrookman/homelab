fe1b49cc0988fafa74f0c61b9791533aa21d75a8
Brian Brookman <bcbrookman@users.noreply.github.com>
2024-06-13 23:26:20 -0700

    Remove pgsql-ct initial password

--- platform-layer/terraform/pgsql-cts.tf
+++ platform-layer/terraform/pgsql-cts.tf
@@ -6,6 +6,5 @@ module "pgsql-ha" {
   net_gateway_addr     = "192.168.20.1"
   net_starting_hostnum = 141
   net_vlan             = 20
-  initial_root_pw      = data.sops_file.tfvars.data["pgsql_initial_root_password"]
   sshkeys              = data.sops_file.tfvars.data["sshkeys"]
 }
